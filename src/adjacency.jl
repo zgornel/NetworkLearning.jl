62c3cec48e54ec10be7bdcb9b9e4d80c85ed16f4
Corneliu Cofaru <cornel@oxoaresearch.com>
2017-11-29 13:02:48 +0100

    Minor changes for adjacency methods

--- src/adjacency.jl
+++ src/adjacency.jl
@@ -224,10 +224,10 @@ adjacency_matrix(f::T, data::S) where {T,S} = adjacency_matrix(f(data))
 
 ###########################################################################
 ### TO REMOVE when division broadcast bug for sparse matrices is pulled ###
-adjacency_matrix(am::T) where T <:SparseMatrixCSC = Matrix(am)#############	
+#adjacency_matrix(am::T) where T <:SparseMatrixCSC = Matrix(am)#############	
 ###########################################################################
 
-adjacency_obs(A::DenseMatrix, r::UnitRange, obsdim::ObsDim.Constant{2}) = view(A,:,r)
-adjacency_obs(A::DenseMatrix, r::UnitRange, obsdim::ObsDim.Constant{1}) = view(A,r,:)
-adjacency_obs(A::SparseMatrixCSC, r::UnitRange, obsdim::ObsDim.Constant{2}) = A[:,r]
-adjacency_obs(A::SparseMatrixCSC, r::UnitRange, obsdim::ObsDim.Constant{1}) = A[r,:]
+adjacency_obs(A::T where T<:AbstractMatrix, r::S where S<:UnitRange, obsdim::ObsDim.Constant{2}) = view(A,:,r)
+adjacency_obs(A::T where T<:AbstractMatrix, r::S where S<:UnitRange, obsdim::ObsDim.Constant{1}) = view(A,r,:)
+adjacency_obs(A::T where T<:SparseMatrixCSC, r::S where S<:UnitRange, obsdim::ObsDim.Constant{2}) = A[:,r]
+adjacency_obs(A::T where T<:SparseMatrixCSC, r::S where S<:UnitRange, obsdim::ObsDim.Constant{1}) = A[r,:]
